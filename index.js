6e76149aed55113703770c6a84dad3af8399312c
SiyogR <rsiyog108@gmail.com>
2024-10-09 17:34:28 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const element = (
+  // Write your code here.
+  <div className='bg-container'>
+    <h1 className='heading'>Congratulations</h1>
+    <div className='container text-center'>
+        <img src='https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png' className="img" />
+        <h1 className='heading1'>Kiran V</h1>
+        <p className='paragraph'>
+          Vishnu Institue of compuer Educatins and Technology
+        </p>
+        <p className='paragraph text-center'>bhimravan</p>
+
+          <img src='https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png'className = "img" />
+        </div>
+      </div>
+    
+
+)
+
+ReactDOM.render(element, document.getElementById('root'))
